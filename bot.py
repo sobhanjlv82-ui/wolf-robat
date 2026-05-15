cf5753a9853b8608b473003c2c9cb966ffec434c
sobhanjlv82-ui <sobhanjlv82@gmail.com>
2026-05-15 11:52:40 +0330

    update

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "توکن_ربات"
+TOKEN = "8222728148:AAGAsSkqTTJxaYPuYiOI_fk80e-8BP9bfic"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ ربات روشن شد")
